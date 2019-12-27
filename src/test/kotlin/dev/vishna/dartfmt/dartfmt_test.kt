071967620b81ae78e63ec38a2a3999fced502f3e
Lukasz Wisniewski <luke.cherrish@gmail.com>
2019-12-27 11:17:10 +0100

    extra sanity check test for kmnd

--- src/test/kotlin/dev/vishna/dartfmt/dartfmt_test.kt
+++ src/test/kotlin/dev/vishna/dartfmt/dartfmt_test.kt
@@ -1,8 +1,15 @@
 package dev.vishna.dartfmt
 
+import dev.vishna.kmnd.execute
+import dev.vishna.kmnd.shList
+import dev.vishna.kmnd.weaveTo
+import dev.vishna.kmnd.weaveToBlocking
 import kotlinx.coroutines.runBlocking
 import org.amshove.kluent.`should be equal to`
+import org.amshove.kluent.`should contain`
 import org.junit.Test
+import java.io.ByteArrayOutputStream
+import java.lang.IllegalStateException
 
 val unformattedDartCode =
 """
@@ -18,6 +25,24 @@ val formattedDartCode =
 """
 
 class DartfmtTest {
+
+    @Test
+    fun kmnd() = runBlocking<Unit> {
+        val dartfmt = "dartfmt --help".shList()
+        val dartOutputStream = ByteArrayOutputStream()
+        val result = dartfmt.execute { inputStream ->
+            inputStream weaveToBlocking dartOutputStream
+        }
+
+        if (result != 0) {
+            throw IllegalStateException("dartfmt returned exit code $result")
+        }
+
+        val formattedOutput = dartOutputStream.toByteArray().toString(Charsets.UTF_8)
+        print(formattedOutput)
+        formattedOutput `should contain` "Idiomatically formats Dart source code"
+    }
+
     @Test
     fun formatting() = runBlocking<Unit> {
         unformattedDartCode.dartfmt() `should be equal to` formattedDartCode
